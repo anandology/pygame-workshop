3f435bc6de5ca4a05768880bb445ed8f41f0eaa2
Anand Chitipothu <anandology@gmail.com>
2017-05-31 11:28:41 +0530

    Added some problems.

--- game0.py
+++ game0.py
@@ -1,3 +1,15 @@
+"""
+Hello world program using python.
+
+This just opens a blank window  with title "Hello world!" and waits for 5 seconds before quitting.
+
+PROBLEMS:
+
+* Try changing the size of the window
+* Try changing the title of the window
+* What happens if you don't have the wait?
+
+"""
 
 import pygame
 
@@ -6,6 +18,7 @@ pygame.init()
 # Set the window size
 size = 400, 400
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Hello world!') 
 
 # wait for 5 seconds so that we can see the window
 pygame.time.wait(5000)
--- game1.py
+++ game1.py
@@ -1,4 +1,33 @@
+"""
+Simple pygame program to draw a circle on the screen.
 
+PROBLEMS:
+
+Problem 1:
+
+* Change the color of the ball to red, blue and gray
+* Try changing the RGB
+
+Problem 2: 
+
+Move the ball to the top-right corner.
+
+Will the ball stay in the top-right corner if the size is changed to something else?
+
+
+Problem 3:
+
+Draw balls in all the 4 the corners.
+
+Problem 4:
+
+Draw a row full of balls.
+
+Problem 5:
+
+Fill the screen with balls.
+
+"""
 import pygame
 pygame.init()
 
